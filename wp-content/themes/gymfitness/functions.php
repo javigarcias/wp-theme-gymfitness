250d992f0fc893d4fffc7f7a51ea4f4f77959721
javigarcias <javigarciaweb@gmail.com>
2021-11-15 15:56:43 +0100

    Implemento hoja de estilos principal

--- wp-content/themes/gymfitness/functions.php
+++ wp-content/themes/gymfitness/functions.php
@@ -1,5 +1,6 @@
 <?php
 
+//Menus de navegacion
 function crossfit_menus() {
     register_nav_menus(array(
         'menu-principal' => __( 'Menu Principal', 'gymcrossfit' )
@@ -8,3 +9,9 @@ function crossfit_menus() {
 
 add_action('init', 'crossfit_menus');
 
+// Scripts y Styles
+function crossfit_scripts_stiles() {
+    //Hoja de estilos principal
+    wp_enqueue_style('style', get_stylesheet_uri(), array(), '1.0.0');
+}
+add_action('wp_enqueue_scripts', 'crossfit_scripts_stiles');
--- wp-content/themes/gymfitness/header.php
+++ wp-content/themes/gymfitness/header.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Entrenamiento Crossfit Valencia</title>
+    <?php wp_head(); ?>
 </head>
 <body>
     
@@ -13,9 +14,15 @@
         <div class="barra-navegacion">
             <div class="logo">
                 <img src="<?php echo get_template_directory_uri() ?>/img/Logo-crossfit.webp" alt="logo crossfit">
-
             </div>
-
+            <?php
+                $args = array(
+                    'theme_location' => 'menu-principal',
+                    'container' => 'nav',
+                    'container_class' => 'menu-principal'
+                );
+                wp_nav_menu($args);
+            ?>
         </div>
 
     </div>
